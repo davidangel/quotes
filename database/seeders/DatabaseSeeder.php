9ee6adaf389d81a605f7df14bdeaf00033de1c1f
David Angel <david@davidangel.net>
2021-03-11 23:54:12 -0500

    Modify seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -25,6 +26,12 @@ class DatabaseSeeder extends Seeder
             'name' => 'David\'s Team',
             'user_id' => 1,
             'personal_team' => 1,
+        ]);
+        DB::table('quotes')->insert([
+            'text' => Inspiring::quote(),
+            'author' => '',
+            'user_id' => 1,
+            'team_id' => 1,
         ]);
     }
 }
